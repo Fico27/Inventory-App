21a4a07bf6a6300640c2d46b0848d31d675f6c15
Phil <182430206+Fico27@users.noreply.github.com>
2025-08-28 10:10:49 +0200

    Added the ability to search for all books as well as specific books.

---
+++ db/bookQuery.js
@@ -0,0 +1,17 @@
+const pool = require("./pool");
+
+async function getAllBooks(searchTerm = "") {
+  let queryText = "SELECT * FROM books";
+  const queryParams = [];
+
+  // Note to self - instead of appending the search term to pool.query we can push
+  // search term into an array IF it is looking for an exact book. Makes it
+  //cleaner and performs santization as needed.
+  if (searchTerm) {
+    queryText += "WHERE books ILIKE $1";
+    queryParams.push(`%${searchTerm}%`);
+  }
+
+  const { rows } = await pool.query(queryText, queryParams);
+  return rows;
+}
